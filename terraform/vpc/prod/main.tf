c514710b6237fe8d3e61c4e2aef993ab45c979d3
jasonblanchard <jas.blanchard@gmail.com>
2021-05-02 16:02:14 -0400

    enable dns

--- terraform/vpc/prod/main.tf
+++ terraform/vpc/prod/main.tf
@@ -11,6 +11,9 @@ module "vpc" {
 #   enable_nat_gateway = true
 #   enable_vpn_gateway = true
 
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
   tags = {
     Terraform = "true"
     Environment = "prod"
